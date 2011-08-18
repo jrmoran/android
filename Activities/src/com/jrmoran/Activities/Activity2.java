afd363fcfbe5a151c310f2b148fb6de7885b0594
jrmoran <jrmoransv@gmail.com>
2011-08-18 00:06:43 -0400

    linking to activities using intents

---
+++ Activities/src/com/jrmoran/Activities/Activity2.java
@@ -0,0 +1,15 @@
+package com.jrmoran.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity2 extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// Using class generated from res/layout/main2.xml
+		setContentView(R.layout.main2);
+	}
+	
+}
--- Activities/src/com/jrmoran/Activities/MainActivity.java
+++ Activities/src/com/jrmoran/Activities/MainActivity.java
@@ -1,8 +1,10 @@
 package com.jrmoran.Activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 
 public class MainActivity extends Activity {
 	
@@ -14,6 +16,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main);
         Log.d(tag, "onCreate()");
     }
+    
+    // Callbacks for standard activities' life cycle events    
     protected void onStart() {
     	super.onStart();
     	Log.d(tag, "onStart()");
@@ -37,5 +41,21 @@ public class MainActivity extends Activity {
 	protected void onDestroy() {
 		super.onDestroy();
 		Log.d(tag, "onDestroy()");
+	}
+	
+	// Linking to Activity2 using an Intent
+	public boolean onKeyDown(int keyCode, KeyEvent event){
+		if(keyCode == KeyEvent.KEYCODE_DPAD_UP){
+			
+			// starting activity by its intent filter name as defined in the manifest file
+			startActivity(new Intent("com.jrmoran.Activity2"));
+		}
+		
+		if(keyCode == KeyEvent.KEYCODE_DPAD_DOWN){
+			// starting activity by its class name. This is limited to activities within 
+			// the same project
+			startActivity(new Intent(this, Activity2.class));
+		}
+		return false;
 	}
 }
